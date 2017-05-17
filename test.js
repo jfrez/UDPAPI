7f07ed08259dfa6bd5db98a599d1cc352933592e
pwned <root@prog2.servidores.eit.udp.cl>
2017-05-16 22:04:00 -0400

    5

--- test.js
+++ test.js
@@ -1,13 +1,16 @@
 var api = require('./api');
 var res="";
+  var user_id = "180206679";
+  var token = "mmae2010";
+
 var logged = function(res,data){
 console.log(data);
 }
-  var user_id = "180206679";
-  var token = "mmae2010";
+
 var login = function(salt,cookie){
  api.login(res,user_id,token,salt,cookie,logged);
 };
+
 api.salt(res,login);
 
 
